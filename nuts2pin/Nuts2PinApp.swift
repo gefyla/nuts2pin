3c23c889c4bba0e598c28c3541359f75bb63271f
gfylak <gfylak@ncsu.edu>
2025-06-12 19:53:18 -0400

    ci: reset

--- nuts2pin/Nuts2PinApp.swift
+++ nuts2pin/Nuts2PinApp.swift
@@ -1,13 +1,10 @@
 import SwiftUI
 
 @main
-struct nuts2pinApp: App {
-    // @StateObject private var viewModel = CourseViewModel() // Temporarily commented out
-    
+struct Nuts2PinApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                // .environmentObject(viewModel) // Temporarily commented out
         }
     }
 } 
